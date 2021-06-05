7fb6dec3e93a142a8bd1ac128145007eaf373374
Ross Stockman <ross.stockman@gmail.com>
2021-06-05 07:41:09 -0500

    initial commit blank project

---
+++ src/main/java/io/sumac/mustache/database/MustacheTemplateFromDatabaseApplication.java
@@ -0,0 +1,13 @@
+package io.sumac.mustache.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MustacheTemplateFromDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MustacheTemplateFromDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/sumac/mustache/database/MustacheTemplateFromDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sumac.mustache.database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MustacheTemplateFromDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
